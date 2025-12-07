9637713636007192c59dd73968b460b12be4411a
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2025-12-07 21:58:23 +0300

    feat: add state to control add-habit icon visibility

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,16 +1,44 @@
 import { Link } from "react-router-dom";
 import { GiSunglasses } from "react-icons/gi";
 import { BsSunglasses } from "react-icons/bs";
+import { LuFilePlus2 } from "react-icons/lu";
+import { useState } from "react";
 function Navbar() {
-  return (
-    <nav className="grid grid-cols-[130px_150px_auto] p-6 bg-[#232020] text-[22px] text-[#319530]">
-      <Link to="/home">Home</Link>
-      <Link to="/add-habit">Add Habit</Link>
+  const [isAddHabitPage, setIsAddHabitPage] = useState(false);
+  console.log(isAddHabitPage);
 
-      <div className="flex items-center justify-end">
-        <BsSunglasses className="text-[#260816] text-3xl" />
-        {/* <GiSunglasses className="text-[34px] text-[#FCF5EE]" /> */}
-      </div>
+  return (
+    <nav className="grid grid-cols-9 p-6 items-center bg-[#232020] text-[22px] text-[#319530]">
+      {isAddHabitPage ? (
+        <>
+          <Link to="/home" onClick={() => setIsAddHabitPage(false)}>
+            Home
+          </Link>
+          <Link to="/add-habit" onClick={() => setIsAddHabitPage(true)}>
+            Add Habit
+          </Link>
+
+          <div className="flex items-center justify-around col-start-9">
+            <LuFilePlus2 className="w-8 h-8" />
+            <BsSunglasses className="text-[#0d0709] text-3xl" />
+            {/* <GiSunglasses className="text-[34px] text-[#FCF5EE]" /> */}
+          </div>
+        </>
+      ) : (
+        <>
+          <Link to="/home" onClick={() => setIsAddHabitPage(false)}>
+            Home
+          </Link>
+          <Link to="/add-habit" onClick={() => setIsAddHabitPage(true)}>
+            Add Habit
+          </Link>
+
+          <div className="flex items-center justify-end col-start-9">
+            <BsSunglasses className="text-[#0d0709] text-3xl mr-[21.5px]" />
+            {/* <GiSunglasses className="text-[34px] text-[#FCF5EE]" /> */}
+          </div>
+        </>
+      )}
     </nav>
   );
 }
