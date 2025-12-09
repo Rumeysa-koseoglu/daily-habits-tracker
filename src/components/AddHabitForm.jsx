f3dfe5b21a757327110bf13aa362d8b7d0432bbb
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2025-12-09 21:22:16 +0300

    feat(form): add form structure and label for habit title

--- src/components/AddHabitForm.jsx
+++ src/components/AddHabitForm.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function AddHabitForm() {
-  return <div></div>;
+  return (
+    <div
+      id="addHabitForm"
+      className="max-w-[500px] h-[470px] p-13 bg-[#232020] text-[#756d6d] rounded-4xl"
+    >
+      <form
+        method="dialog"
+        className="flex flex-col gap-14 box-content items-center justify-center"
+      >
+        <div className="flex flex-col items-center gap-4">
+          <label className=""> HABIT TITLE: </label>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default AddHabitForm;
