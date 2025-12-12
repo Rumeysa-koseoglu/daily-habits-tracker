4b231f1e5505e237869f166b4fe110b753281638
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2025-12-12 21:51:43 +0300

    feat: update layout structure and center AddHabitForm

--- src/pages/AddHabitPage.jsx
+++ src/pages/AddHabitPage.jsx
@@ -4,10 +4,16 @@ import AddHabitForm from "../components/AddHabitForm";
 
 function AddHabitPage({ isForm, setIsForm }) {
   return (
-    <div>
-      <HabitItem />
-      {isForm && <AddHabitForm setIsForm={setIsForm} />}
-    </div>
+    <>
+      <div className="habit-items h-screen pt-[119px] px-10 grid grid-cols-3 gap-6 content-start">
+        <HabitItem />
+      </div>
+      {isForm && (
+        <div className="flex min-h-screen w-screen justify-center items-center">
+          <AddHabitForm setIsForm={setIsForm} />
+        </div>
+      )}
+    </>
   );
 }
 
