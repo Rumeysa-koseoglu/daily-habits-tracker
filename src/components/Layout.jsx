d5731b4047972cffe1222affa103b0042e3c9508
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2025-12-12 21:41:46 +0300

    style: update size of layout component's main container

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,11 @@
-import React from "react";
 import Navbar from "../pages/Navbar";
 
 function Layout({ children, onHandleToggleForm }) {
   return (
     <>
       <Navbar onHandleToggleForm={onHandleToggleForm} />
-      <div className="max-w-full min-h-[calc(100vh-81px)] p-4 m-0 bg-[#565050]">
-        <main>{children}</main>
+      <div>
+        <main className="max-w-screen h-screen bg-[#565050]">{children}</main>
       </div>
     </>
   );
